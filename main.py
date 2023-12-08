d2c20a157da0f19962195ef065549deae146ee47
Omar Omran <omaromran2003@gmail.com>
2023-12-08 18:22:13 +0200

    add the topics

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import random
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class sessionHour(BaseModel):
+    what_hour_is_the_session: str | int
 
 @app.post("/")
-def read_root():
-    return {"message": f"YOU HAVE OPENED THE PORTAL, RUN WITH THIS CODE {random.randint(200,505)} !"}
-
-@app.post("/send-your-cat")
-def send_your_cat():
-    
-    return {"message": f"YOU HAVE OPENED THE PORTAL, RUN WITH THIS CODE {random.randint(200,505)} !"}
+def read_root(body: sessionHour):
+    if body.what_hour_is_the_session in ["02:00", "14:00", "2:00", 2, 14]:
+        return {
+            "message": f"YOU HAVE OPENED THE PORTAL, Take this code {random.randint(200, 505)} and RUN!",
+            "topics": [
+                "What the hell is backend",
+                "How do you communicate with the backend?",
+                "API architecture styles",
+                "REST APIs",
+                "Databases",
+                "Deployment"
+            ]
+        }
+    return {"message": f"PORTAL IS CLOSED, TRY A DIFFERENT TIME? !"}
